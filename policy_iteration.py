11029d57fce7654431798dd81cbfe5509b9c90c1
Minchan-Kim <k21mchan@gmail.com>
2019-10-11 20:58:33 +0900

    content correction

--- policy_iteration.py
+++ policy_iteration.py
@@ -36,45 +36,46 @@ class Environment:
         return next_state
 
 
-class ValueIteration:
+class PolicyIteration:
     def __init__(self, environment):
         self.environment = environment
         self.value_table = [[0.0] * 5 for _ in range(5)]
         self.policy_table = [[[0.25] * 4 for _ in range(5)] for _ in range(5)]
 
-    def value_iteration(self):
-        """Do a value iteration once."""
+    def policy_evaluation(self):
+        """Do a policy evaluation once."""
         states = self.environment.get_states()
         actions = self.environment.get_actions()
         for row in states:
             for state in row:
+                value = 0
                 if (state == (2, 2)):
                     continue
-                action_value = []
-                for action in actions:
+                for action_index, action in enumerate(actions):
+                    temp = 0
                     next_state = self.environment.get_next_state(state, action)
-                    q = self.environment.get_reward(state, action) + 0.9 * self.value_table[next_state[0]][next_state[1]]
-                    action_value.append(q)
-                self.value_table[state[0]][state[1]] = max(action_value)
+                    temp += self.policy_table[state[0]][state[1]][action_index]
+                    temp *= (self.environment.get_reward(state, action) + 0.9 * self.value_table[next_state[0]][next_state[1]])
+                    value += temp
+                self.value_table[state[0]][state[1]] = value
 
-    def policy_determination(self):
-        """Determines a optimal policy given the values of states."""
+    def policy_improvement(self):
+        """Do a policy improvement once."""
         states = self.environment.get_states()
         actions = self.environment.get_actions()
         for row in states:
             for state in row:
-                action_value = []
+                q = []
                 for action in actions:
                     next_state = self.environment.get_next_state(state, action)
-                    q = self.environment.get_reward(state, action) + 0.9 * self.value_table[next_state[0]][next_state[1]]
-                    action_value.append(q)
-                max_action_value = max(action_value)
-                cnt = action_value.count(max_action_value)
-                for index in range(len(actions)):
-                    if (action_value[index] == max_action_value):
-                        self.policy_table[state[0]][state[1]][index] = 1.0 / cnt
+                    q.append(self.environment.get_reward(state, action) + 0.9 * self.value_table[next_state[0]][next_state[1]])
+                max_q = max(q)
+                cnt = q.count(max_q)
+                for action_index in range(len(actions)):
+                    if q[action_index] == max_q:
+                        self.policy_table[state[0]][state[1]][action_index] = (1.0 / cnt)
                     else:
-                        self.policy_table[state[0]][state[1]][index] = 0
+                        self.policy_table[state[0]][state[1]][action_index] = 0.0
 
     def get_difference(self, table1, table2):
         """Returns 1-norm of a difference between the two value tables."""
@@ -99,12 +100,12 @@ class ValueIteration:
             print(lst)
 
     def print_policy(self):
-        """Prints optimal policy of all states.
+        """Prints policy of all states.
         Prints 1 if an agent takes the action.
         Otherwise prints 0.
         Order of actions : [up, down, left, right]
         """
-        print("Optimal Policy")
+        print("Policy Table")
         for row in self.policy_table:
             lst = []
             for policy in row:
@@ -120,22 +121,22 @@ class ValueIteration:
 
 
 def main():
-    print("Value Iteration")
-    agent = ValueIteration(Environment())
+    print("Policy Iteration")
+    agent = PolicyIteration(Environment())
     old_value = agent.get_value()
-    threshold = 0.001
+    threshold = 0.01
     max_iteration = 50
     for cnt in range(max_iteration):
-        print("iteration {}".format(cnt + 1))
-        agent.value_iteration()
+        print("Iteration {}".format(cnt + 1))
+        agent.policy_evaluation()
         agent.print_value()
+        agent.policy_improvement()
         new_value = agent.get_value()
         if (agent.get_difference(new_value, old_value) < threshold):
             break
         old_value = new_value
         if (cnt == 49):
             print("Iteration Failed")
-    agent.policy_determination()
     agent.print_policy()
 
 
