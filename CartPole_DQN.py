82616663a5eb94edbbb13dc1635f68cad8efc825
Minchan-Kim <k21mchan@gmail.com>
2019-11-23 15:57:11 +0900

    CartPole-v0 solution

--- CartPole_DQN.py
+++ CartPole_DQN.py
@@ -11,16 +11,18 @@ from collections import deque
 class DQN:
     def __init__(self, env):
         self.epsilon = 1.0
-        self.epsilon_discount_rate = 0.99
-        self.epsilon_minimum = 0.01
+        self.epsilon_discount_rate = 0.999
+        self.epsilon_minimum = 0
         self.batch_size = 64
-        self.minimum_data = 100
+        self.minimum_data = 500
         self.discount_rate = 0.99
-        self.step_size = 0.001
-
+        self.step_size = 0.005
+        self.target_fix = 0
+        self.count = 0
         self.env = env
         self.model = self.build_model()
         self.target_model = self.build_model()
+        self.update()
         self.replay_buffer = deque(maxlen = 2000)
 
     def build_model(self):
@@ -29,17 +31,14 @@ class DQN:
         model.add(layers.Dense(24, activation = "relu"))
         model.add(layers.Dense(24, activation = "relu"))
         model.add(layers.Dense(self.env.action_space.n, activation = "linear"))
-        model.compile(optimizer = "sgd", loss = "mse")
+        model.compile(optimizer = keras.optimizers.SGD(learning_rate = self.step_size, momentum = 0.5), loss = "mse")
         return model
 
     def learn(self):
         if (len(self.replay_buffer) < self.minimum_data):
             return
         replays = random.sample(self.replay_buffer, self.batch_size)
-        input_data = []
-        for replay in replays:
-            input_data.append(replay[0])
-        input_data = np.stack(input_data)
+        input_data = np.array([replay[0] for replay in replays])
         target_data = self.model.predict(input_data)
         for i in range(self.batch_size):
             state, action, reward, next_state, done = replays[i]
@@ -51,6 +50,11 @@ class DQN:
         self.model.fit(input_data, target_data, batch_size = self.batch_size, epochs = 1, verbose = 0)
         if (self.epsilon >= self.epsilon_minimum):
             self.epsilon *= self.epsilon_discount_rate
+        if (self.target_fix > 0):
+            self.count += 1
+            if (self.target_fix == self.count):
+                self.update()
+                self.count = 0
 
     def update(self):
         self.target_model.set_weights(self.model.get_weights())
@@ -61,30 +65,51 @@ class DQN:
         else:
             return random.randrange(self.env.action_space.n)
 
-    def run_episode(self):
+    def run_episode(self, learn = True):
         state = self.env.reset()
-        score = 0
+        reward_total = 0
+        temp = 0
+        if not learn:
+            temp = self.epsilon
+            self.epsilon = 0
         while True:
             action = self.take_action(state)
             next_state, reward, done, info = self.env.step(action)
             self.replay_buffer.append((state, action, reward, next_state, done))
-            score += reward
-            self.learn()
+            reward_total += reward
+            if learn:
+                self.learn()
             if done:
                 break
-            state = next_state
-        self.update()
-        return score
+            state = next_state        
+        if learn and (not (self.target_fix > 0)):
+            self.update()
+        if not learn:
+            self.epsilon = temp
+        return reward_total
 
 
 def main():
     env = gym.make("CartPole-v0")
     agent = DQN(env)
-    max_iteration = 100
-    score = 0
+    max_iteration = 500
+    max_episode = 100
+    rewards = []
+    reward = 0
     for i in range(max_iteration):
-        score = agent.run_episode()
-        print("Score of episode {}: {}".format((i + 1), score))
+        reward = agent.run_episode()
+        print("Reward of episode {:3}: {}".format((i + 1), reward))
+        if (reward == 200):
+            rewards.clear()
+            for j in range(max_episode):
+                reward = agent.run_episode(learn = False)
+                rewards.append(reward)
+                print("Reward of trial {:3}: {}".format((j + 1), reward))
+                if (sum(rewards) < (max_episode * (env.spec.reward_threshold + 2 * j - 198))):
+                    break
+            if (sum(rewards) >= (env.spec.reward_threshold * max_episode)):
+                print("CartPole-v0 solved!")
+                break    
     env.close()
 
 if __name__ == "__main__":
